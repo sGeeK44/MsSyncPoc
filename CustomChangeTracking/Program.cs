6b7cfc0ddb93ffc43867fbff7eb6c28874ac811b
PERIN François <f.perin@zero-gachis.com>
2017-04-14 18:28:02 +0200

    Initial commit

---
+++ CustomChangeTracking/Program.cs
@@ -0,0 +1,238 @@
+using System;
+using System.Data.SqlServerCe;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Data;
+using Microsoft.Synchronization.Data.SqlServerCe;
+using Sync;
+
+namespace CustomChangeTracking
+{
+    class Program
+    {
+        //Return the client connection string with the password.
+        public const string ConnStrSqlCeClientSync = @"Data Source='SyncSampleClient.sdf'";
+
+        //Return the server connection string. 
+        public const string ConnStrDbServerSync = @"Data Source='SyncSampleServer.sdf'";
+        public const string ScopeName = "ProductsScope";
+
+        //Delete the client database.
+        public static void RecreateCompactDatabase()
+        {
+            Utility.CreateDatabase(ConnStrSqlCeClientSync);
+            Utility.CreateDatabase(ConnStrDbServerSync);
+        }
+
+        private static void Main()
+        {
+            LocalSyncUncouplingSerialize();
+        }
+
+        public static void LocalSyncUncouplingSerialize()
+        {
+            RecreateCompactDatabase();
+            ProvisionDb();
+
+            // create a connection to the SyncCompactDB database
+            var clientProvider = CreateProvider(ConnStrSqlCeClientSync);
+
+            // subscribe for errors that occur when applying changes to the client
+            clientProvider.ApplyChangeFailed += Program_ApplyChangeFailed;
+
+            // create a connection to the SyncDB server database
+            var serverProvider = CreateProvider(ConnStrDbServerSync);
+            
+            Utility.MakeDataChangesOnServer(ConnStrSqlCeClientSync);
+            
+            // execute the synchronization process
+            SyncLocalWithSerialize(clientProvider, serverProvider);
+            Console.WriteLine(String.Empty);
+            Console.WriteLine("End...");
+            Console.ReadKey();
+        }
+
+        private static void SyncLocalWithSerialize(SqlCeSyncProvider clientProvider, SqlCeSyncProvider serverProvider)
+        {
+            SyncLocalWithSerializeOnWay(serverProvider, clientProvider);
+            SyncLocalWithSerializeOnWay(clientProvider, serverProvider);
+        }
+
+        private static void SyncLocalWithSerializeOnWay(SqlCeSyncProvider clientProvider, SqlCeSyncProvider serverProvider)
+        {
+            uint batchSize;
+            SyncKnowledge knowledge;
+            object changedData;
+            var clientSession = new SyncSessionContext(clientProvider.IdFormats, new SyncCallbacks());
+            var serverSession = new SyncSessionContext(serverProvider.IdFormats, new SyncCallbacks());
+            clientProvider.BeginSession(SyncProviderPosition.Local, clientSession);
+            serverProvider.BeginSession(SyncProviderPosition.Remote, serverSession);
+
+            serverProvider.GetSyncBatchParameters(out batchSize, out knowledge);
+            var clientBatchSize = (uint)Transfert(batchSize);
+            var clientknowledge = (SyncKnowledge)Transfert(knowledge);
+            var changeBatch = clientProvider.GetChangeBatch(clientBatchSize, clientknowledge, out changedData);
+
+            var serverchangeBatch = (ChangeBatch)Transfert(changeBatch);
+            var serverChangedData = Transfert(changedData);
+            var syncStats = new SyncSessionStatistics();
+            serverProvider.ProcessChangeBatch(ConflictResolutionPolicy.DestinationWins, serverchangeBatch, serverChangedData, new SyncCallbacks(), syncStats);
+            serverProvider.EndSession(serverSession);
+            clientProvider.EndSession(clientSession);
+
+            // print statistics
+            Console.WriteLine("ChangesApplied: " + syncStats.ChangesApplied);
+            Console.WriteLine("ChangesFailed: " + syncStats.ChangesFailed);
+        }
+
+        private static object Transfert(object o)
+        {
+            var mem = SerializeToStream(o);
+            return DeserializeFromStream(mem);
+        }
+
+        public static MemoryStream SerializeToStream(object o)
+        {
+            var stream = new MemoryStream();
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, o);
+            return stream;
+        }
+
+        public static object DeserializeFromStream(MemoryStream stream)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            stream.Seek(0, SeekOrigin.Begin);
+            var o = formatter.Deserialize(stream);
+            return o;
+        }
+
+        public static void LocalSyncUncoupling()
+        {
+            RecreateCompactDatabase();
+            ProvisionDb();
+
+            // create a connection to the SyncCompactDB database
+            var clientProvider = CreateProvider(ConnStrSqlCeClientSync);
+
+            // subscribe for errors that occur when applying changes to the client
+            clientProvider.ApplyChangeFailed += Program_ApplyChangeFailed;
+
+            // create a connection to the SyncDB server database
+            var serverProvider = CreateProvider(ConnStrDbServerSync);
+
+            Utility.MakeDataChangesOnServer(ConnStrSqlCeClientSync);
+
+            // execute the synchronization process
+            SyncLocal(clientProvider, serverProvider);
+            Console.WriteLine(String.Empty);
+            Console.WriteLine("End...");
+            Console.ReadKey();
+        }
+
+        private static SqlCeSyncProvider CreateProvider(string db)
+        {
+            return Utility.CreateProvider(db, ScopeName);
+        }
+
+        private static void SyncLocal(SqlCeSyncProvider clientProvider, SqlCeSyncProvider serverProvider)
+        {
+            SyncLocalOnWay(serverProvider, clientProvider);
+            SyncLocalOnWay(clientProvider, serverProvider);
+        }
+
+        private static void SyncLocalOnWay(SqlCeSyncProvider clientProvider, SqlCeSyncProvider serverProvider)
+        {
+            uint batchSize;
+            SyncKnowledge knowledge;
+            object changedData;
+            var clientSession = new SyncSessionContext(clientProvider.IdFormats, new SyncCallbacks());
+            var serverSession = new SyncSessionContext(serverProvider.IdFormats, new SyncCallbacks());
+            clientProvider.BeginSession(SyncProviderPosition.Local, clientSession);
+            serverProvider.BeginSession(SyncProviderPosition.Remote, serverSession);
+            serverProvider.GetSyncBatchParameters(out batchSize, out knowledge);
+            var changeBatch = clientProvider.GetChangeBatch(batchSize, knowledge, out changedData);
+
+            var syncStats = new SyncSessionStatistics();
+            serverProvider.ProcessChangeBatch(ConflictResolutionPolicy.DestinationWins, changeBatch, changedData,
+                new SyncCallbacks(), syncStats);
+            serverProvider.EndSession(serverSession);
+            clientProvider.EndSession(clientSession);
+
+            // print statistics
+            Console.WriteLine("ChangesApplied: " + syncStats.ChangesApplied);
+            Console.WriteLine("ChangesFailed: " + syncStats.ChangesFailed);
+        }
+
+        public void LocalSync()
+        {
+            RecreateCompactDatabase();
+            ProvisionDb();
+
+            // create a connection to the SyncCompactDB database
+            var clientConn = new SqlCeConnection(ConnStrSqlCeClientSync);
+
+            // create a connection to the SyncDB server database
+            var serverConn = new SqlCeConnection(ConnStrDbServerSync);
+
+            // create the sync orhcestrator
+            var syncOrchestrator = new SyncOrchestrator();
+
+            // set local provider of orchestrator to a CE sync provider associated with the 
+            // ProductsScope in the SyncCompactDB compact client database
+            var clientProvider = new SqlCeSyncProvider("ProductsScope", clientConn);
+            // subscribe for errors that occur when applying changes to the client
+            clientProvider.ApplyChangeFailed += Program_ApplyChangeFailed;
+            syncOrchestrator.LocalProvider = clientProvider;
+
+            // set the remote provider of orchestrator to a server sync provider associated with
+            // the ProductsScope in the SyncDB server database
+            var serverProvider = new SqlCeSyncProvider("ProductsScope", serverConn);
+            syncOrchestrator.RemoteProvider = serverProvider;
+
+            // set the direction of sync session to Upload and Download
+            syncOrchestrator.Direction = SyncDirectionOrder.UploadAndDownload;
+
+
+            Utility.MakeDataChangesOnServer(ConnStrSqlCeClientSync);
+
+            // execute the synchronization process
+            var syncStats = syncOrchestrator.Synchronize();
+
+            // print statistics
+            Console.WriteLine("Start Time: " + syncStats.SyncStartTime);
+            Console.WriteLine("Total Changes Uploaded: " + syncStats.UploadChangesTotal);
+            Console.WriteLine("Total Changes Downloaded: " + syncStats.DownloadChangesTotal);
+            Console.WriteLine("Complete Time: " + syncStats.SyncEndTime);
+            Console.WriteLine(String.Empty);
+            Console.ReadKey();
+        }
+
+        private static void ProvisionDb()
+        {
+            var scopeDesc = ProvisionMainDb();
+            ProvisionningClient(scopeDesc);
+        }
+
+        private static DbSyncScopeDescription ProvisionMainDb()
+        {
+            return Utility.ProvisionServer(ConnStrDbServerSync, ScopeName);
+        }
+
+        private static void ProvisionningClient(DbSyncScopeDescription scopeDesc)
+        {
+            Utility.ProvisionningClient(ConnStrSqlCeClientSync, ScopeName, scopeDesc);
+        }
+
+        static void Program_ApplyChangeFailed(object sender, DbApplyChangeFailedEventArgs e)
+        {
+            // display conflict type
+            Console.WriteLine(e.Conflict.Type);
+
+            // display error message 
+            Console.WriteLine(e.Error);
+        }
+    }
+}
---
+++ Sync/Utility.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data.SqlServerCe;
+using System.IO;
+using Microsoft.Synchronization.Data;
+using Microsoft.Synchronization.Data.SqlServerCe;
+
+namespace Sync
+{
+    public class Utility
+    {
+        //Make server changes that are synchronized on the second 
+        //synchronization.
+        public static void MakeDataChangesOnServer(string db)
+        {
+            int rowCount = 0;
+
+            using (var serverConn = new SqlCeConnection(db))
+            {
+                serverConn.Open();
+                var sqlCommand = serverConn.CreateCommand();
+                sqlCommand.CommandText = "INSERT INTO Customer (CustomerName) VALUES ('Cycle Mart');";
+                rowCount += sqlCommand.ExecuteNonQuery();
+                sqlCommand.CommandText = "UPDATE Customer SET  SalesPerson = 'James Bailey' WHERE CustomerName = 'Tandem Bicycle Store'";
+                rowCount += sqlCommand.ExecuteNonQuery();
+                sqlCommand.CommandText = "DELETE FROM Customer WHERE CustomerName = 'Sharp Bikes'";
+                rowCount += sqlCommand.ExecuteNonQuery();
+                serverConn.Close();
+            }
+
+            Console.WriteLine("Rows inserted, updated, or deleted at the server: " + rowCount);
+        }
+
+        public static void CreateDatabase(string path)
+        {
+            using (var clientConn = new SqlCeConnection(path))
+            {
+                if (File.Exists(clientConn.Database))
+                {
+                    File.Delete(clientConn.Database);
+                }
+            }
+
+            var sqlCeEngine = new SqlCeEngine(path);
+            sqlCeEngine.CreateDatabase();
+            CreateTable(path);
+        }
+
+        private static void CreateTable(string path)
+        {
+            using (var clientConn = new SqlCeConnection(path))
+            {
+                clientConn.Open();
+                using (var command = clientConn.CreateCommand())
+                {
+                    command.CommandText = "CREATE TABLE [Customer] (Id integer PRIMARY KEY IDENTITY, CustomerName nvarchar (200), SalesPerson nvarchar (200)); ";
+                    command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO Customer (CustomerName, SalesPerson) VALUES ('Bike Mart', 'FPE');";
+                    command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO Customer (CustomerName, SalesPerson) VALUES ('Tandem Bicycle Store', 'FPE');";
+                    command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO Customer (CustomerName, SalesPerson) VALUES ('Sharp Bikes', 'FPE');";
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static DbSyncScopeDescription ProvisionServer(string db, string scopeName)
+        {
+            var serverConn = new SqlCeConnection(db);
+
+            // define a new scope named ProductsScope
+            var scopeDesc = new DbSyncScopeDescription(scopeName);
+
+            // get the description of the Products table from SyncDB dtabase
+            var tableDesc = SqlCeSyncDescriptionBuilder.GetDescriptionForTable("Customer", serverConn);
+
+            // add the table description to the sync scope definition
+            scopeDesc.Tables.Add(tableDesc);
+
+            // create a server scope provisioning object based on the ProductScope
+            var serverProvision = new SqlCeSyncScopeProvisioning(serverConn, scopeDesc);
+
+            // skipping the creation of table since table already exists on server
+            serverProvision.SetCreateTableDefault(DbSyncCreationOption.Skip);
+
+            // start the provisioning process
+            serverProvision.Apply();
+
+            // get the description of ProductsScope from the SyncDB server database
+            return SqlCeSyncDescriptionBuilder.GetDescriptionForScope(scopeName, serverConn);
+        }
+
+
+        public static void ProvisionningClient(string db, string scopeName, DbSyncScopeDescription scopeDesc)
+        {
+            // create a connection to the SyncCompactDB database
+            var clientConn = new SqlCeConnection(db);
+
+            // create CE provisioning object based on the ProductsScope
+            var clientProvision = new SqlCeSyncScopeProvisioning(clientConn, scopeDesc);
+
+            // starts the provisioning process
+            clientProvision.Apply();
+        }
+
+        public static SqlCeSyncProvider CreateProvider(string db, string scopeName)
+        {
+            var clientConn = new SqlCeConnection(db);
+            var clientProvider = new SqlCeSyncProvider(scopeName, clientConn);
+            return clientProvider;
+        }
+    }
+}
